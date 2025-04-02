a2e03e7f142b3715aa7d8baf7813f764639e0a09
Tioluwani Enoch Olubunmi <teolubunmi@kali.teolubunmi>
2025-04-02 10:28:05 -0500

    added a rock paper scissors game to the C learning language

---
+++ programming-c/c-calculator/calculator.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int sum(int a, int b)
+{
+    return a + b;
+}
+
+int sub(int a, int b)
+{
+    return a - b;
+}
+
+int mul(int a, int b)
+{
+    return a * b;
+}
+
+float div(int a, int b)
+{
+    return a / b;
+}
+
+int mod(int a, int b)
+{
+    return a % b;
+}
+
+int main()
+{
+    printf("%d\n", sum(10, 21));
+    printf("%d\n", sub(10, 21));
+    printf("%d\n", mul(10, 21));
+    printf("%d\n", div(10, 21));
+    printf("%d\n", mod(10, 21));
+
+    return 0;
+}
---
+++ programming-c/rock-paper-scissors/rock-papaer-scissors.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+void greeting()
+{
+    printf("How're you doing?? Do you wanna play ROCK PAPER SCISSORS?\n");
+}
+
+int printRandom(int min, int max)
+{
+    return rand() % (max - min + 1) + min;
+}
+
+char* attack(int foo)
+{
+    if (foo == 1) return "rock";
+    if (foo == 2) return "paper";
+    if (foo == 3) return "scissors";
+    return "invalid";
+}
+
+char* checkWin(char* userChoice)
+{
+    int compChoice = printRandom(1, 3);
+    printf("Computer chose: %s\n", attack(compChoice));
+
+    if (strcmp(userChoice, attack(compChoice)) == 0) // note that 0 in the strcmp means that it is equal too.
+        return "That's a draw!";
+    else if ((strcmp(userChoice, "rock") == 0 && strcmp(attack(compChoice), "scissors") == 0) ||
+             (strcmp(userChoice, "paper") == 0 && strcmp(attack(compChoice), "rock") == 0) ||
+             (strcmp(userChoice, "scissors") == 0 && strcmp(attack(compChoice), "paper") == 0))
+        return "You won!";
+    else
+        return "You lost!";
+}
+
+int main()
+{
+    srand(time(0)); // Seed the random number generator
+    greeting();
+
+    char userChoice[10];
+    printf("Enter rock, paper, or scissors: ");
+    scanf("%s", userChoice);
+
+    printf("%s\n", checkWin(userChoice));
+
+    return 0;
+}
