046f5f6747a3084393d57d5ffdb199fb901d91fd
CodeMaestro-dev <codemaestro101@gmail.com>
2025-08-28 17:15:24 -0500

    revisited my C learning code for refresher in C and also made some rewrites

--- programming-c/c-calculator/calculator.c
+++ programming-c/c-calculator/calculator.c
@@ -30,7 +30,7 @@ int main()
     printf("%d\n", sum(10, 21));
     printf("%d\n", sub(10, 21));
     printf("%d\n", mul(10, 21));
-    printf("%d\n", div(10, 21));
+    printf("%f\n", div(21, 10));
     printf("%d\n", mod(10, 21));
 
     return 0;
--- programming-c/helloworld.c
+++ programming-c/helloworld.c
@@ -4,5 +4,6 @@ int main()
 {
     printf("Hello World!!\n");
     printf("Kinda, like C honestly has a lot of similarity with Java!!!!!\n");
+
     return 0;
 }
